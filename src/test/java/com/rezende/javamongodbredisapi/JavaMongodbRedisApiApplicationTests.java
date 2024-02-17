2baae4f3bb7021551f383117ee7f2eacd0020409
Adriano <adrianopr@ciandt.com>
2024-02-17 11:51:05 -0500

    commit inicial

---
+++ src/test/java/com/rezende/javamongodbredisapi/JavaMongodbRedisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezende.javamongodbredisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongodbRedisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
